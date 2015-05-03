79edfde16ee7ad4a5d56d6f6dc015322ed2f2941
jul101 <jul19770621@gmail.com>
2015-05-04 07:48:10 +0800

    修改Doris版型

--- doris/js/doris.js
+++ doris/js/doris.js
@@ -1,3 +1,7 @@
+var DORIS_CSV_FILES={
+  brides:"./data/brides.csv"
+};
+
 function test(){
   console.log(123);
 }
@@ -36,9 +40,71 @@ function initialPhoto(){
   });
 }
 
+
+function loadBrides(error, rows){
+  console.log('data done',rows);
+  var count=rows.length;
+  var eachRowCellCount=2;
+  var getOneDataHtml=function(data,eachRowCellCount){
+    var htmlStr="";
+    //htmlStr+="<div class='folio-item' style='transform: translate3d(0px, 0px, 0px);'>";
+    //htmlStr+="<div class='folio-item isotope-item web packaging' style='position: absolute; left: 0px; top: 0px; transform: translate3d(0px, 0px, 0px);'>";
+    //htmlStr+="<div class='folio-item isotope-item web packaging' style='position: absolute; left: 0px; top: 0px; transform: translate3d("+(eachRowCellCount*300)+"px, 0px, 0px);'>";
+    //htmlStr+="<div class='col-md-3'>";
+    htmlStr+="<div class='col-md-2 col-sm-6 no-padding folio-item'>";
+    htmlStr+="	<a href='./"+data.thumbImage+"' class='prettyPhoto'>";
+    htmlStr+="		<div class='folio-thumb'>";
+    htmlStr+="			<img src='./"+data.image+"' class='img-responsive' alt=''>";
+    htmlStr+="		</div>";
+    htmlStr+="	</a>";
+    htmlStr+="	<div class='folio-overlay'>";
+    htmlStr+="		<h4><a href='#'>"+data.title+"</a></h4>";
+    htmlStr+="	</div>";
+    htmlStr+="	<i class='icon-uni17D'></i>";
+    htmlStr+="</div>";
+    return htmlStr;
+  };
+
+  var rowHtml="";
+  for(var i=0;i<count;i++){
+    rowHtml+=getOneDataHtml(rows[i],i);
+  }
+  $("#da-thumbs").append(rowHtml);
+
+}
+
+function handleHeaderPosition() {
+  var CurrentHeaderPosition = $(".header").offset().top;// current header's vertical position
+
+  console.log(CurrentHeaderPosition);
+
+  var headerFix = function(){
+    console.log('headerFix',CurrentHeaderPosition);
+    var CurrentWindowPosition = $(window).scrollTop();// current vertical position
+    if (CurrentWindowPosition > CurrentHeaderPosition) {
+      $(".header").addClass("fixNav");
+    } else {
+      $(".header").removeClass("fixNav");
+    }
+  };
+
+  headerFix();// call headerFix() when the page was loaded
+  if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
+    $(window).bind("touchend touchcancel touchleave", function(e){
+      headerFix();
+    });
+  } else {
+    $(window).scroll(function() {
+      headerFix();
+    });
+  }
+}
+
 $(document).ready(function(){
   $('.carousel').carousel({
     interval: 5000 //changes the speed
   });
-  initialPhoto();
+  d3.csv(DORIS_CSV_FILES.brides,function(d){return d;},loadBrides);
+  handleHeaderPosition();
+  //initialPhoto();
 });
